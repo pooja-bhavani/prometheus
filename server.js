4fdf95bc22ef99dde753fbcc11c91c393629e905
Pooja bhavani <147735975+pooja-bhavani@users.noreply.github.com>
2026-02-28 19:55:11 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const data = fs.readFileSync("index.html");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(data);
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
